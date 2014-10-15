2594e0afdaa7c2a028a108e50438a5c4404c1514
Andrew Thomson <athomsonguy@gmail.com>
2014-10-15 10:20:18 -0700

    2nd try at SafeOutput.

--- safeoutput/__init__.py
+++ safeoutput/__init__.py
@@ -3,9 +3,25 @@ from os.path import abspath, dirname
 from sys import stdout
 from tempfile import NamedTemporaryFile
 
-class SafeOutput:
-  def __init__(self, dst=None):
+
+def SafeOutputFile(dst=None):
+  if dst:
+    fd = NamedTemporaryFile(dir=dirname(abspath(dst)))
+  else:
+    fd = stdout
+  return _SafeOutputWrapper(fd, dst)
+
+class _SafeOutputWrapper:
+  def __init__(self, fd, dst):
+    self.fd = fd
     self.dst = dst
+
+  def rename(self, dst):
+    self.dst = dst
+
+  def __enter__(self):
+    self.fd.__enter__()
+    return self
 
   def __getattr__(self, name):
     # Attribute lookups are delegated to the underlying tempfile
@@ -13,18 +29,18 @@ class SafeOutput:
     a = getattr(fd, name)
     return a
 
-  def __enter__(self):
-    if self.dst is None:
-      self.fd = stdout
-    else:
-      self.fd = NamedTemporaryFile(dir=dirname(abspath(self.dst)))
-    return self
+  def close(self, commit=True):
+    if self.dst is not None and commit == True:
+      rename(self.fd.name, self.dst)
+      self.fd.delete = False
+    self.fd.close()
 
   def __exit__(self, exc_type, exc_value, traceback):
-    if exc_value is None:
-      if self.dst is not None:
-        # Try to commit the file.
-        rename(self.fd.name, self.dst)
-        self.fd.delete = False
+    self.close(exc_value is None)
     return self.fd.__exit__(exc_type, exc_value, traceback)
 
+  def __del__(self):
+    # If we get to __del__ and have not already committed,
+    # we don't know that the output is safe. Allow
+    # tempfile to delete the file.
+    self.close(False)
