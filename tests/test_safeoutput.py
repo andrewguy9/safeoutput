737b53eb8b81ae8d0acf93e6a123b60517fbf447
Andrew Thomson <athomsonguy@gmail.com>
2019-12-22 10:34:33 -0800

    Add a with style stdout test.

--- tests/test_safeoutput.py
+++ tests/test_safeoutput.py
@@ -112,3 +112,10 @@ def test_write_stdout_after_close(capsys):
     out,err = capsys.readouterr()
     assert out == file_data + file_data
     assert err == ""
+
+def test_stdout_with_success_str():
+    file_data = u"testoutput"
+    mode = "w"
+    with safeoutput.open(None, mode) as f:
+        f.write(file_data)
+    # Verify stdout.
