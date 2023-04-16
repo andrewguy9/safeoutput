c4670e696a7390d8456e351ebf965da1add5dfdd
Andrew Thomson <athomsonguy@gmail.com>
2023-04-16 13:11:40 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ tests_require = ['tox', 'pytest']
 
 setup(
     name='safeoutput',
-    version='2.0',
+    version='2.1',
     description='Tempfile wrapper to ensure output files are either complete, or empty. Also handles stdout, which gives no truncation guarantees.',
     long_description='Tool to facilitate console script output redirection. When scripts run, often they have an output file option. If no output option is specified, its common to write to stdout. Its common to use tempfiles as output, and then rename the tempfile to the output name as the last step of the program so that the flip of output is atomic and partial/truncated/corrupt output is not confused as successful output. This is especially true when dealing with make, as exiting with error will stop make, but subsequent runs will assume that partial output files left in the workspace are complete. Writing to stdout gives no guarantees about partial results due to truncation.',
     url='http://github.com/andrewguy9/safeoutput',
